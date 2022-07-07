48432328f778783d7e24081170bacd3ff57ee12e
Emin Israfilzadeh <israfilzadehemin@gmail.com>
2022-07-07 17:26:39 +0400

    GATEWAY: Add filter

--- flighty-api-gateway/src/main/java/com/example/flightyapigateway/FlightyApiGatewayApplication.java
+++ flighty-api-gateway/src/main/java/com/example/flightyapigateway/FlightyApiGatewayApplication.java
@@ -2,14 +2,20 @@ package com.example.flightyapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import security.SecurityFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(value = {
+        SecurityFilter.Config.class
+})
 public class FlightyApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlightyApiGatewayApplication.class, args);
     }
+
 
 }
