2e1f68051a2bcc71df4b697de99311113e6d3473
Emin Israfilzadeh <israfilzadehemin@gmail.com>
2022-07-13 08:40:29 +0400

    MS-FLIGHT: create flight ms

---
+++ ms-flight/src/main/java/com/tutorials/msflight/MsFlightApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.msflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFlightApplication.class, args);
+    }
+
+}
