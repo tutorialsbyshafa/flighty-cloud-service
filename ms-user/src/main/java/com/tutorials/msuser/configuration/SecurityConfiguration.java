0cea6344889a84ee6a051f67ad7f380ecb8e8895
Shafa00 <sefammmdv00@gmail.com>
2022-07-17 15:06:15 +0400

    USER-MS: add security config

---
+++ ms-user/src/main/java/com/tutorials/msuser/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.tutorials.msuser.configuration;
+
+import com.tutorials.msuser.security.AuthorizationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@AllArgsConstructor
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final String[] AUTH_WHITELIST = {
+            "/authenticate",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/webjars/**",
+            "/actuator/**"
+    };
+    private final AuthorizationFilter authorizationFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+}
