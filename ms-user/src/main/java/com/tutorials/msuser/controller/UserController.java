e0f65ba63ff076b302e74a8d56eead856ca900f3
Emin Israfilzadeh <israfilzadehemin@gmail.com>
2022-07-18 21:53:47 +0400

    MS-USER: Change response status

--- ms-user/src/main/java/com/tutorials/msuser/controller/UserController.java
+++ ms-user/src/main/java/com/tutorials/msuser/controller/UserController.java
@@ -5,19 +5,24 @@ import com.tutorials.msuser.model.SignupResponseModel;
 import com.tutorials.msuser.service.UserService;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/ms-user")
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignupResponseModel> signup(@Valid @RequestBody SignupRequestModel request) {
         var response = userService.signup(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
 }
