d7a9adacbe5681cd90d7971e59376f87f3b603e5
Emin Israfilzadeh <israfilzadehemin@gmail.com>
2022-07-18 20:56:29 +0400

    MS-USER: add method to mapper

--- ms-user/src/main/java/com/tutorials/msuser/service/impl/UserServiceImpl.java
+++ ms-user/src/main/java/com/tutorials/msuser/service/impl/UserServiceImpl.java
@@ -1,27 +1,20 @@
 package com.tutorials.msuser.service.impl;
 
-import com.tutorials.msuser.entity.User;
 import com.tutorials.msuser.exception.AppException;
 import com.tutorials.msuser.mapper.UserMapper;
 import com.tutorials.msuser.model.SignupRequestModel;
 import com.tutorials.msuser.model.SignupResponseModel;
-import com.tutorials.msuser.repository.RoleRepository;
 import com.tutorials.msuser.repository.UserRepository;
 import com.tutorials.msuser.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collections;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
-    private final BCryptPasswordEncoder encoder;
     @Override
     public SignupResponseModel signup(SignupRequestModel request) {
         checkUserUniqueness(request.getUsername());
@@ -30,17 +23,10 @@ public class UserServiceImpl implements UserService {
         validateFullname(request.getFullName());
 
         var user = userMapper.mapRequestToEntity(request);
-        user.setPassword(encoder.encode(request.getPassword()));
-        setRoles(user);
 
         userRepository.save(user);
 
         return userMapper.mapEntityToResponse(user);
-    }
-
-    private void setRoles(User user) {
-        user.setRoles(Collections.singletonList(roleRepository.findByNameIgnoreCase("ROLE_USER")
-                .orElseThrow(() -> new AppException("role not found"))));
     }
 
     private void checkUserUniqueness(String email) {
